6e9e61629faf360057773ee45fb26b73ec82f115
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-02-27 14:26:15 -0400

    variavel dividendo agora se chama numrecebido

--- java/Espelho.java
+++ java/Espelho.java
@@ -5,28 +5,18 @@ class Espelho
     public static void main(String[] args)
     {
         Scanner Leitor = new Scanner(System.in);
-        long dividendo,divisor,restop;
+        long numrecebido,divisor,restop;
         boolean parada;
         divisor = 10;
         restop = 0;
         
         System.out.println("Digite um numero ");
-        dividendo = Leitor.nextLong();
+        numrecebido = Leitor.nextLong();
         do
         {
-            System.out.print((dividendo%divisor));
-            restop = (dividendo/divisor);
-            dividendo = (dividendo/divisor);
-        /*
-            if(restop == 0)
-            {
-                parada = true;
-            }
-            else
-            {
-                parada = false;
-            }
-        */
+            System.out.print((numrecebido%divisor));
+            restop = (numrecebido/divisor);
+            numrecebido = (numrecebido/divisor);
         }
         while(restop != 0);
 
