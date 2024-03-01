41b27e25f8aef8dbe6ed1349c17d2d2ecfaadcad
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-01 16:43:41 -0400

    Criado Descrescor.java
    Arquivo criado, codigo escrito. Otimizações opcionais

---
+++ java/Decrescor.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+class Descrescor
+{
+    public static void main(String[] args)
+    {
+        Scanner Leitor = new Scanner(System.in);
+        int v1,v2,v3;
+
+        System.out.println("Digite um numero: ");
+        v1 = Leitor.nextInt();
+
+        System.out.println("Digite outro numero: ");
+        v2 = Leitor.nextInt();
+
+        System.out.println("Digite o ultimo numero: ");
+        v3 = Leitor.nextInt();
+
+        if(v1>v2)
+        {
+            if(v1>v3)
+            {
+                System.out.print(v1+" ");
+
+                if(v3>v2)
+                {
+                    System.out.print(v3+" ");
+                    System.out.print(v2);
+                }
+                else
+                {
+                    System.out.print(v2+" ");
+                    System.out.print(v3);
+                }
+            }
+            else
+            {
+                System.out.print(v3+" ");
+                System.out.print(v1+" ");
+                System.out.print(v2);
+                /*if(v3>v2)
+                {
+                    System.out.print(v3+" ");
+                    System.out.print(v2);
+                }
+                else
+                {
+                    System.out.print(v2+" ");
+                    System.out.print(v3);
+                }
+                */
+            }
+        }
+        else
+        {
+            if(v2>v3)
+            {
+                System.out.print(v2+" ");
+
+                if(v1>v3)
+                {
+                    System.out.print(v1+" ");
+                    System.out.print(v3);
+                }
+                else
+                {
+                    System.out.print(v3+" ");
+                    System.out.print(v1);
+                }
+            }
+            else
+            {
+                System.out.print(v3+" ");
+                System.out.print(v2+" ");
+                System.out.print(v1);
+
+                /* 
+                if(v3>v1)
+                {
+                    System.out.print(v3);
+                }
+                else
+                {
+                    System.out.println(v1);
+                }
+                */
+            }
+        }
+
+        Leitor.close();
+    }
+}
