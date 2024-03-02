925b86b44afae6a1e52305a1422dc51d91f816f2
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-02 13:59:55 -0400

    Prodemgrp.java codificado

--- java/Prodemgrp.java
+++ java/Prodemgrp.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 class Prodemgrp
 {
     public static void main(String[] argds)
     {
-        
+        Scanner Leitor = new Scanner(System.in);
+        int codigo;
+
+        System.out.println("Insira o codigo do produto:");
+        codigo = Leitor.nextInt();
+
+        if(codigo == 1)
+        {
+            System.out.println("Alimento não-perecivel");
+        }
+
+        if(codigo >= 2 & codigo <= 4) 
+        {
+            System.out.println("Alimento perecivel");
+        }
+
+        if(codigo == 5 | codigo == 6)
+        {
+            System.out.println("Vestuario");
+        }
+
+        if(codigo == 7)
+        {
+            System.out.println("Higiene pessoal");
+        }
+
+        if(codigo >= 8 & codigo <= 15)
+        {
+            System.out.println("Limpeza e utensilios domesticos");
+        }
+
+        if(codigo > 15)
+        {
+            System.out.println("Inválido");
+        }
+
+        Leitor.close();
     }
 }
