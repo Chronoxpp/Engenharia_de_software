c8fc4d869dc688b2c4bcbb2dc7346d50ab6da951
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-02 15:06:49 -0400

    inserido codigo da calculado.java, necessita de desenvolvimento

--- java/Calculadora.java
+++ java/Calculadora.java
@@ -1,1 +1,58 @@
+import java.util.Scanner;
 
+class Calculadora
+{
+    public static void main(String[] argds)
+    {
+        Scanner Leitor = new Scanner(System.in);
+        int v1,v2,resultado;
+        //float resultado;
+        String operador;
+
+        System.out.println("Insira o primeiro numero");
+        v1 = Leitor.nextInt();
+        System.out.println("Insira a operação");
+        operador = Leitor.next();
+        System.out.println("Insira o segundo numero");
+        v2 = Leitor.nextInt();
+
+
+        if(operador.equals("+"))
+        {
+            resultado = v1 + v2;
+            System.out.println("Resultado: "+resultado);
+        }
+
+        if(operador.equals("-"))
+        {
+            resultado = v1 - v2;
+            System.out.println("Resultado: "+resultado);
+        }
+
+        if(operador.equals("*"))
+        {
+            resultado = v1 * v2;
+            System.out.println("Resultado: "+resultado);
+        }
+
+        if(operador.equals("/"))
+        {
+            resultado = v1 / v2;
+            System.out.println("Resultado: "+resultado);
+        }
+
+        if(operador.equalsIgnoreCase("r"))
+        {
+            //resultado = v1 + v2;
+            System.out.println("Resultado: em desenvolvimento!");
+        }
+
+        if(operador.equals("**"))
+        {
+            //resultado = v1 * v2;
+            System.out.println("Resultado: em desenvolvimento!");
+        }
+
+        Leitor.close();
+    }
+}
