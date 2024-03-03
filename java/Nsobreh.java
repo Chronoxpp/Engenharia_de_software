dbe40596f37da447af5a21b4e2417645dce5e70a
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-03 08:48:55 -0400

    Criado Nsobreh.java
    o arquivo foi criado e codificado. Otimizações opcionais

---
+++ java/Nsobreh.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Nsobreh{
+    public static void main(String[] args){
+        Scanner Leitor = new Scanner(System.in);
+        int v1;
+        float h; 
+        System.out.print("Digite um numero: ");
+        v1 = Leitor.nextInt();
+        h = 0;
+
+        for(float i = 1; i <= v1; i = i +1){
+            h = h + (1/i);
+        }
+
+        System.out.print("O resultado é: "+h);
+
+        Leitor.close();
+    }
+}
