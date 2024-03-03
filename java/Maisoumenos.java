31d50957175b49de0d2ba6a16f0531bd672c73c0
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-03 10:48:27 -0400

    criado maisoumenos.java

---
+++ java/Maisoumenos.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Maisoumenos
+{
+    public static void main(String[] agrs)
+    {
+        Scanner Leitor = new Scanner(System.in);
+        float maior,menor,atual;
+        int numeros,i;
+        maior = 0;
+        menor = 0;
+        
+        System.out.println("Quantos numeros você deseja usar?");
+        numeros = Leitor.nextInt();
+
+        for(i = 1; i <= numeros;i = i +1)
+        {
+            System.out.print("Digite um numero: ");
+            atual = Leitor.nextFloat();
+            if(i == 1)
+            {
+                maior = atual;
+                menor = atual;
+            }
+            else
+            {
+                if(atual > maior)
+                {
+                    maior = atual;
+                }
+                if(atual < menor)
+                {
+                    menor = atual;
+                }
+            }
+        }
+
+        System.out.println(" ");
+        System.out.print("maior numero: "+maior+" e menor numero: "+menor);
+
+        Leitor.close();
+    }
+}
