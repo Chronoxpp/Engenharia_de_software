d467545ca815a4eb3fa70e35639a45c3fa86af07
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-07 08:56:26 -0400

    Questao15.java criado e codificado
    O algoritmo esta funcional, melhorias de usabilidade e funcionamento do código podem ser interessantes

---
+++ java/Questao15.java
@@ -0,0 +1,20 @@
+class Questao15
+{
+    public static void main(String[] args)
+    {
+        int result = 0;
+        int numero = 2;
+        for(int d1 = 1; d1 <= 6; d1 = d1 + 1)
+        {
+            for(int d2 = 1; d2 <= 6; d2 = d2 + 1)
+            {
+                if(d1+d2 == numero)
+                {
+                    result = result + 1;
+                }
+                //System.out.println(d1+"+"+d2+"= "+(d1+d2)+" ");
+            }
+        }
+        System.out.print(result+" ou "+result+"/36 ou aproximadamente "+((result*100)/36)+"%");
+    }
+}
