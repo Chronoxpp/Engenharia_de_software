1bfb27fe6f5c46ef68b177275edd2afe3203cea6
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-07 10:06:00 -0400

    Questão12.java revisada e codificada
    otimizações opcionais, necessário revisão para melhor estruturar o codigo

--- java/Questao12.java
+++ java/Questao12.java
@@ -1,13 +1,93 @@
 import java.util.Scanner;
 
-class Mmcemdc
+class Questao12
 {
     public static void main(String[] args)
     {
-        Scanner Leitor = new Scanner(System.in);
-        int v1,v2;
+        Scanner leitor = new Scanner(System.in);
+        int v1,v2,result,divisor,cont;
         boolean parar = false;
 
-        while()
+        System.out.print("Digite o primeiro numero: ");
+        v1 = leitor.nextInt();
+        System.out.print("Digite o primeiro numero: ");
+        v2 = leitor.nextInt();
+
+        cont = 0;
+        divisor = 2;
+        result = 1;
+        while(parar == false)
+        {
+            if(v1%divisor == 0)
+            {
+                cont = cont + 1;
+                v1 = v1/divisor;
+            }
+
+            if(v2%divisor == 0)
+            {
+                cont = cont + 1;
+                v2 = v2/divisor;
+            }
+
+            if(cont > 0)
+            {
+                result = result * divisor;
+            }
+            else
+            {
+                divisor = divisor + 1;
+                if(v1 == 1 && v2 == 1)
+                {
+                    parar = true;
+                }
+            }
+            cont = 0;
+        }
+        System.out.println("MMC = "+result);
+
+        System.out.print("Digite o primeiro numero: ");
+        v1 = leitor.nextInt();
+        System.out.print("Digite o primeiro numero: ");
+        v2 = leitor.nextInt();
+
+        cont = 0;
+        divisor = 2;
+        result = 1;
+        parar = false;
+        while(parar == false)
+        {
+            if(v1%divisor == 0)
+            {
+                cont = cont + 1;
+                v1 = v1/divisor;
+            }
+
+            if(v2%divisor == 0)
+            {
+                cont = cont + 1;
+                v2 = v2/divisor;
+            }
+
+            if(cont == 2)
+            {
+                result = result * divisor;
+            }
+            else
+            {
+                if((v1%divisor) != 0 && (v2%divisor) != 0)
+                {
+                  divisor = divisor + 1;  
+                }
+                if(v1 == 1 && v2 == 1)
+                {
+                    parar = true;
+                }
+            }
+            cont = 0;
+        }
+        System.out.println("MDC = "+result);
+
+        leitor.close();
     }
 }
