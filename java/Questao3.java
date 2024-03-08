3cabecbc0ff6537f44e47c294a230eea2840a3e9
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-07 20:41:34 -0400

    atualizado variavel "codigo" para "idade"

--- java/Questao3.java
+++ java/Questao3.java
@@ -5,37 +5,37 @@ class Questao3
     public static void main(String[] argds)
     {
         Scanner leitor = new Scanner(System.in);
-        int codigo;
+        int idade;
 
-        System.out.println("Insira a idade do nadador");
-        codigo = leitor.nextInt();
+        System.out.print("Insira a idade do nadador: ");
+        idade = leitor.nextInt();
 
-        if(codigo < 5) 
+        if(idade < 5) 
         {
             System.out.println("Invalido");
         }
 
-        if(codigo >= 5 & codigo <= 7) 
+        if(idade >= 5 & idade <= 7) 
         {
             System.out.println("Infantil A");
         }
 
-        if(codigo >= 8 & codigo <= 10) 
+        if(idade >= 8 & idade <= 10) 
         {
             System.out.println("Infantil B");
         }
 
-        if(codigo >= 11 & codigo <= 13) 
+        if(idade >= 11 & idade <= 13) 
         {
             System.out.println("Juvenil A");
         }
 
-        if(codigo >= 14 & codigo <= 17) 
+        if(idade >= 14 & idade <= 17) 
         {
             System.out.println("Juvenil B");
         }
 
-        if(codigo > 18)
+        if(idade > 18)
         {
             System.out.println("Adulto");
         }
