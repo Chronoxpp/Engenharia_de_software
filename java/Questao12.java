cdba2ad8b8e4c2e83684b5d484669b4bd6e3fdf8
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-10 16:06:32 -0400

    alterado variaveis "cont","v1","v2" para "divisOcorridas","n1","n2"

--- java/Questao12.java
+++ java/Questao12.java
@@ -5,86 +5,81 @@ class Questao12
     public static void main(String[] args)
     {
         Scanner leitor = new Scanner(System.in);
-        int v1,v2,result,divisor,cont;
+        int n1,n2,result,divisor,divisOcorridas;
         boolean parar = false;
 
         System.out.print("Digite o primeiro numero: ");
-        v1 = leitor.nextInt();
-        System.out.print("Digite o primeiro numero: ");
-        v2 = leitor.nextInt();
+        n1 = leitor.nextInt();
+        System.out.print("Digite o segundo numero: ");
+        n2 = leitor.nextInt();
 
-        cont = 0;
+        divisOcorridas = 0;
         divisor = 2;
         result = 1;
         while(parar == false)
         {
-            if(v1%divisor == 0)
+            if(n1%divisor == 0)
             {
-                cont = cont + 1;
-                v1 = v1/divisor;
+                divisOcorridas = divisOcorridas + 1;
+                n1 = n1/divisor;
             }
 
-            if(v2%divisor == 0)
+            if(n2%divisor == 0)
             {
-                cont = cont + 1;
-                v2 = v2/divisor;
+                divisOcorridas = divisOcorridas + 1;
+                n2 = n2/divisor;
             }
 
-            if(cont > 0)
+            if(divisOcorridas > 0)
             {
                 result = result * divisor;
             }
             else
             {
                 divisor = divisor + 1;
-                if(v1 == 1 && v2 == 1)
+                if(n1 == 1 && n2 == 1)
                 {
                     parar = true;
                 }
             }
-            cont = 0;
+            divisOcorridas = 0;
         }
         System.out.println("MMC = "+result);
 
-        System.out.print("Digite o primeiro numero: ");
-        v1 = leitor.nextInt();
-        System.out.print("Digite o primeiro numero: ");
-        v2 = leitor.nextInt();
-
-        cont = 0;
+        divisOcorridas = 0;
         divisor = 2;
         result = 1;
         parar = false;
         while(parar == false)
         {
-            if(v1%divisor == 0)
+            if(n1%divisor == 0)
             {
-                cont = cont + 1;
-                v1 = v1/divisor;
+                divisOcorridas = divisOcorridas + 1;
+                n1 = n1/divisor;
             }
 
-            if(v2%divisor == 0)
+            if(n2%divisor == 0)
             {
-                cont = cont + 1;
-                v2 = v2/divisor;
+                divisOcorridas = divisOcorridas + 1;
+                n2 = n2/divisor;
             }
 
-            if(cont == 2)
+            if(divisOcorridas == 2)
             {
                 result = result * divisor;
             }
             else
             {
-                if((v1%divisor) != 0 && (v2%divisor) != 0)
+                if((n1%divisor) != 0 && (n2%divisor) != 0)
                 {
                   divisor = divisor + 1;  
                 }
-                if(v1 == 1 && v2 == 1)
+                if(n1 == 1 && n2 == 1)
                 {
                     parar = true;
                 }
             }
-            cont = 0;
+            divisOcorridas = 0;
         }
         System.out.println("MDC = "+result);
 
