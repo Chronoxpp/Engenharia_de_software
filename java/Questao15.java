6a3768187c90ffc3f34904db0603ff1a6a0a7b21
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-10 16:07:45 -0400

    pequeno ajuste

--- java/Questao15.java
+++ java/Questao15.java
@@ -3,7 +3,7 @@ class Questao15
     public static void main(String[] args)
     {
         int result = 0;
-        int numero = 2;
+        int numero = 7;
         for(int d1 = 1; d1 <= 6; d1 = d1 + 1)
         {
             for(int d2 = 1; d2 <= 6; d2 = d2 + 1)
