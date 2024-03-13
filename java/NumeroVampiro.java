8e41709f5c343aa32f0d3af2846e373efee5871b
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-13 09:58:22 -0400

    primeiras tentativas de formar pares a partir do possível vampiro

--- java/NumeroVampiro.java
+++ java/NumeroVampiro.java
@@ -5,18 +5,42 @@ class NumeroVampiro
     public static void main(String[] args)
     {
         Scanner leitor = new Scanner(System.in);
-        int possivelVampiro,numeroDeAlgarismos,aux;
+        int possivelVampiro,numeroDeAlgarismos,aux,combinacao1,combinacao2;
+        String strCombinacao1,strCombinacao2;
 
         possivelVampiro = leitor.nextInt();
 
         aux = possivelVampiro;
         numeroDeAlgarismos = 0;
-
         for(numeroDeAlgarismos = 1;aux/10 != 0; numeroDeAlgarismos = numeroDeAlgarismos+1)
         {
             aux = aux/10;
         }
-        System.out.print("acabou "+numeroDeAlgarismos);
+
+        if(numeroDeAlgarismos%2 != 0)
+        {
+            System.out.print("Insira um numero com uma quantidade par de digitos!");
+        }
+        else
+        {
+            aux = possivelVampiro;
+            strCombinacao1 = "";
+            strCombinacao2 = "";
+            for(int i = 1; i <= numeroDeAlgarismos ; i = i + 1)
+            {
+                if(i <= (numeroDeAlgarismos/2))
+                {
+                    strCombinacao1 = strCombinacao1 + Integer.toString(aux%10);
+                }
+                else
+                {
+                    strCombinacao2 = strCombinacao2 + Integer.toString(aux%10);
+                }
+                aux = aux/10;
+            }
+            System.out.println("acabou "+strCombinacao1);
+            System.out.println("acabou "+strCombinacao2);
+        }
 
         leitor.close();
     }
