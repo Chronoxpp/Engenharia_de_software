93270a10eb9d117859ca60b18147a8b0eaf5f7a9
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-03-26 17:12:54 -0400

    Update Teste.java

--- java/Teste.java
+++ java/Teste.java
@@ -2,9 +2,6 @@ class Teste
 {
     public static void main(String[] args)
     {
-        for (int i = 1; i<=3 ; i++)
-        {
-            
-        }
+        System.out.println("ola");
     }
 }
