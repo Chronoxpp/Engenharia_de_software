158a960c250d6e6e5fb039dfcd1e10ae5dbf4fdd
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-09-06 11:52:47 -0400

    Desenvolvi uma versao mais estavel do desafio 19, removi um metodo redundante

--- java/semestre1/exericios_do_livro/capitulo_8/desafio_19/Sistema.java
+++ java/semestre1/exericios_do_livro/capitulo_8/desafio_19/Sistema.java
@@ -1,7 +1,7 @@
 //Desenvolvido por Lucas Dourado Rodrigues
 
 public class Sistema {
-
+    //
     public int[] determinarInterseccaoEntreConjuntos(int[] vetor1, int[] vetor2)
     {
         if(vetor1 == null || vetor2 == null)
@@ -9,35 +9,41 @@ public class Sistema {
             return null;
         }
 
-
-        int[] vetorInterseccao = new int[DeterminarQtdeNumerosNaIntersecacao(vetor1,vetor2)];
+        int[] vetorInterseccaoTemporario = new int[vetor1.length];
         int contador = 0;
 
-        if(vetorInterseccao.length <= 0)
+        if(vetorInterseccaoTemporario.length <= 0)
         {
             return null;
         }
 
-
-        for(int i = 0; i < vetor1.length; i += 1)
+        //Preenche o vetor intersecacao temporario
+        for(int vetor1_i = 0; vetor1_i < vetor1.length; vetor1_i += 1)
         {
-            int OcorrenciasDoNumero = 0;
-            for(int j = 0; j < vetor2.length; j += 1)
+            for(int vetor2_j = 0; vetor2_j < vetor2.length; vetor2_j += 1)
             {
-                if(vetor1[i] == vetor2[j])
+                if(vetor1[vetor1_i] == vetor2[vetor2_j])
                 {
-                    if(verificarSeNumeroExisteNoConjunto(vetorInterseccao, vetor1[i]) == false)
+                    if(verificarSeNumeroExisteNoConjunto(vetorInterseccaoTemporario, vetor1[vetor1_i]) == false)
                     {
-                        vetorInterseccao[contador] = vetor1[i];
+                        vetorInterseccaoTemporario[contador] = vetor1[vetor1_i];
                         contador += 1;
                     }
                 }
             }
         }
 
+        //Preenche o vetor interseccao final
+        int[] vetorInterseccao = new int[contador -1];
+        for(int i = 0; i < contador - 1; i += 1)
+        {
+            vetorInterseccao[i] = vetorInterseccaoTemporario[i];
+        }
+
         return vetorInterseccao;
     }
 
+    //
     public boolean verificarSeNumeroExisteNoConjunto(int[] vetor, int numero)
     {
         int ocorrenciasDoNumero = 0;
@@ -57,40 +63,7 @@ public class Sistema {
         return false;
     }
 
-
-    public int DeterminarQtdeNumerosNaIntersecacao(int[] vetor1, int[] vetor2)
-    {
-        int qtdeDeNumerosNaInterseccao = 0;
-        int[] vetorNumerosEncontrados = new int[vetor1.length];
-        int contador = 0;
-
-        for(int i = 0; i < vetor1.length; i += 1)
-        {
-            int OcorrenciasDoNumero = 0;
-            for(int j = 0; j < vetor2.length; j += 1)
-            {
-                if(vetor1[i] == vetor2[j])
-                {
-                    OcorrenciasDoNumero += 1;
-                    System.out.println("Comparou: " + vetor1[i] + " com " + vetor2[j]);
-                }
-            }
-
-            if(OcorrenciasDoNumero > 0)
-            {
-                if(verificarSeNumeroExisteNoConjunto(vetorNumerosEncontrados, vetor1[i]) == false)
-                {
-                    vetorNumerosEncontrados[contador] = vetor1[i];
-                    contador += 1;
-                    qtdeDeNumerosNaInterseccao += 1;
-                }
-
-            }
-        }
-
-        return qtdeDeNumerosNaInterseccao;
-    }
-
+    //
     public String converterVetorInteiroEmMsg(int[] vetor)
     {
         if(vetor == null)
