c9af8813d746889b366cac0d229ec0a55116f355
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-09-14 17:18:31 -0400

    Adicionei um metodo para obter nome do produto a partir de entradas do usuario

--- java/semestre1/exercicios_avaliativos/2_2/Sistema.java
+++ java/semestre1/exercicios_avaliativos/2_2/Sistema.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,7 +8,7 @@ public class Sistema {
 
     public void cadastrarProduto()
     {
-
+        //Nome produto,valor e estoque
     }
 
     public void realizarVenda()
@@ -18,5 +19,25 @@ public class Sistema {
     public void realizarParcelamento(Venda venda)
     {
 
+    }
+
+    public String obterNomeProduto()
+    {
+        String nome = null;
+
+        while(nome == null)
+        {
+            nome = JOptionPane.showInputDialog("Digite o nome do produto: ");
+
+            if(nome == null)
+            {
+                JOptionPane.showMessageDialog(null, "Tente digitar o nome novamente !!");
+                continue;
+            }
+
+            nome = nome.trim();
+        }
+
+        return nome;
     }
 }
