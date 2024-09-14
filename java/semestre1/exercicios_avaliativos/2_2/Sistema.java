fd2dee0feb318df60ea014916fd50f21ce336b8a
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-09-14 17:11:51 -0400

    Criei a classe Sistema

---
+++ java/semestre1/exercicios_avaliativos/2_2/Sistema.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sistema {
+    private List<Venda> vendas = new ArrayList<>();
+    private List<Produto> produtos = new ArrayList<>();
+
+    public void cadastrarProduto()
+    {
+
+    }
+
+    public void realizarVenda()
+    {
+
+    }
+
+    public void realizarParcelamento(Venda venda)
+    {
+
+    }
+}
