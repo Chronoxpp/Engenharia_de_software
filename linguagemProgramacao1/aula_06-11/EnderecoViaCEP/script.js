bd0bc55efe3851c6794203ad05ce6dc611d11283
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2024-11-13 11:09:28 -0400

    Refiz o codigo para algo mais proximo da POO

--- linguagemProgramacao1/aula_06-11/EnderecoViaCEP/script.js
+++ linguagemProgramacao1/aula_06-11/EnderecoViaCEP/script.js
@@ -12,45 +12,54 @@ class Endereco
 
     consultarEnderecoViaCEP()
     {
-        
+        if(this.CEP.length !== 8) {return;}
+
+        if((typeof this.CEP) !== 'string') 
+        {
+            alert('Digite um CEP valido !!');
+            return;
+        };
+
+        return fetch(`https://viacep.com.br/ws/${this.CEP}/json/`)
+            .then(response => response.json())
+            .then(dados => {
+                if(dados.erro) 
+                {
+                    alert('CEP invalido ou endereco inexistente !!');
+                    return;
+                }
+
+                // Atualizando as propriedades do objeto
+                this.UF = dados.uf;
+                this.cidade = dados.localidade;
+                this.bairro = dados.bairro;
+                this.rua = dados.logradouro;
+                this.numero = ''; // Número não vem na API
+            });
     }
 }
+
 
 const inputCEP = document.querySelector('#cep');
 inputCEP.value = inputCEP.value.replace(/\D/g, '');
 
 inputCEP.addEventListener('input', () => {
+    inputCEP.value = inputCEP.value.replace(/\D/g, '');
+
     let CEP = inputCEP.value;
-    if (CEP.length == 8)
-    {
-        consultarEnderecoViaCEP(CEP);
-    }
+
+    let endereco = new Endereco(CEP);
+
+    endereco.consultarEnderecoViaCEP().then(() => {
+        popularInputsEndereco(endereco);
+    });
 });
-
-
-function consultarEnderecoViaCEP(CEP)
-{
-    if(typeof CEP != 'string') {return};
-
-    if (CEP.length != 8) {return};
-    
-    fetch(`https://viacep.com.br/ws/${CEP}/json/`)
-        .then(response => response.json())
-        .then(dados => {
-            if(dados.erro) return;
-            
-            let endereco = new Endereco(CEP, dados['uf'], dados['localidade'], dados['bairro'], dados['logradouro'], undefined)
-
-            popularInputsEndereco(endereco);
-        })
-
-}
 
 
 function popularInputsEndereco(endereco)
 {
-    document.querySelector('#cep').value = endereco['cep'];
-    document.querySelector('#uf').value = endereco['UF'];
+    document.querySelector('#cep').value = endereco['CEP'];
+    document.querySelector('#uf').value = endereco.UF;
     document.querySelector('#cidade').value = endereco['cidade'];
     document.querySelector('#bairro').value = endereco['bairro'];
     document.querySelector('#rua').value = endereco['rua'];
